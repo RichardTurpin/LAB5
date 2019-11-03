a5f9330880193b7275189a92ca1f355290698786
RichardTurpin <a61213@ualg.pt>
2019-11-03 23:49:40 +0000

    Update register.php

--- register.php
+++ register.php
@@ -8,13 +8,24 @@ $smarty->compile_dir = 'templates_c';
 $MessagError=typeError($_GET[$Error]);
 function typeError($Error)
 {
-if($Error==0)
-{
+switch ($Error) {
+	case '0':
+		$MessagError="Todos os campos devem ser preenchidos";
+		break;
+	case '1':
+		$MessagError="Email já existe na base de dados";
+		break;
+	case '2':
+		$MessagError="Email tem formato incorrecto";
+		break;
+	case '3':
+		$MessagError="Password em branco";
+		break;
+	case '4':
+		$MessagError="Passwords não coincidem";
+		break;
 
 }
-
-
-
 
 return $MessagError;
 
