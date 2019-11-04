396a434e907172239a2c9ab50794b5bdd63cfff2
RichardTurpin <a61213@ualg.pt>
2019-11-04 16:39:48 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -9,26 +9,33 @@ $smarty->compile_dir = 'templates_c';
 $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 if($db)
 {
-
+	print_r($_POST);
+	print_r(empty($_POST['name']));
 	if(empty($_POST['name']) or empty($_POST['email']) or empty($_POST['password']) or empty($_POST['password_confirmation']) ) 
 	{
-   		header("Location: register.php?Error=0");   
+   		//header("Location: register.php?Error=0");   
   	}
   	$name  = $_POST['name'];
   	$password = substr(md5($_POST['password']),0,32);
   	$email    = $_POST['email'];
 	$query = "SELECT * FROM users WHERE email = '$email'";
 	$result = @ mysql_query($query,$db);
-
- 	if(  $_POST['password'] == '' &&
+ 	if(mysql_num_rows($result) > 0)
+ 	 {
+ 	 	print_r($query."<br>");
+ 	 	print_r($_POST."<br>");
+ 	 	print_r(empty($_POST['name']));
+      	//header("Location: register.php?Error=1");
+  	 }
+ 	elseif(  $_POST['password'] == '' &&
      $_POST['password_confirmation']=='' && isset($_POST['name']) && isset($_POST['email']))
   	{
-  		header("Location: register.php?Error=3&name=name&email=email");  
+  		//header("Location: register.php?Error=3&name=name&email=email");  
 
   	}
   	elseif(  $_POST['password'] != $_POST['password_confirmation'] )
   	{
-  		header("Location: register.php?Error=4&name=name&email=email");  
+  		//header("Location: register.php?Error=4&name=name&email=email");  
 
   	}
 	else
