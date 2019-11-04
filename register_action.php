c6cb3e074abf41cf9d089103d3ea8d3ee703fba6
RichardTurpin <a61213@ualg.pt>
2019-11-04 02:05:21 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -28,7 +28,7 @@ if($db)
  	elseif(  $_POST['password'] == '' &&
      $_POST['password_confirmation']=='' && isset($_POST['name']) && isset($_POST['email']))
   	{
-  		header("Location: register.php?$Error=3&name=$_POST['name']&email=$_POST['email']");  
+  		header("Location: register.php?Error=3&name=$_POST['name']&email=$_POST['email']");  
 
   	}
   	elseif(  $_POST['password'] != $_POST['password_confirmation'] )
