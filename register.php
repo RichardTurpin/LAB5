2434d9793aa74b5b4923c9f0bb5511e8fcbd2c02
RichardTurpin <a61213@ualg.pt>
2019-11-04 01:31:15 +0000

    changes

--- register.php
+++ register.php
@@ -5,6 +5,7 @@ $smarty = new Smarty();
 $smarty->template_dir = 'templates';
 $smarty->compile_dir = 'templates_c';
 $smarty->assign("MessagError",$MessagError);
+$smarty->assign('href0',"register.php");
 
 $MessagError=typeError($_GET[$Error]);
 function typeError($Error)
